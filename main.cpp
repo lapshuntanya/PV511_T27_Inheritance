1c72797478dacd38dcc404075dde85d87b7ca2ba
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-23 19:19:45 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include "Lesson.h"
 
 int main() {
+
+    Lesson a;
+    a.showInfo();
+
+    Lesson b("C++", "Inheritance", "Create a base class!");
+    b.showInfo();
+
 
     return 0;
 }
