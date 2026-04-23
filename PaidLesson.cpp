5f99ed52a43d98a3555defa70ca4caf89186b1f0
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-23 20:02:58 +0300

    PaidLesson

--- PaidLesson.cpp
+++ PaidLesson.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "PaidLesson.h"
+
+
+PaidLesson::PaidLesson() {
+ //Lesson(); "empty",  "empty",  "empty"
+  price = 0;
+  cout << "PaidLesson(): " << theme << endl;
+}
+
+ PaidLesson::PaidLesson(string theme, string info, string home, float price)
+  :Lesson(theme, info, home)
+{
+  //Lesson(); "empty",  "empty",  "empty"
+  /*this->theme = theme;
+  this->info = info;
+  setHome(home);*/
+
+  this->price = price;
+  cout << "PaidLesson(t, i, h, p): " << theme << endl;
+}
+
+ PaidLesson::~PaidLesson() {
+  cout << "~PaidLesson(): " << theme << endl;
+}
+
+void PaidLesson::setPrice(float price) {
+  this->price = price;
+}
+
+float PaidLesson::getPrice() const {
+  return price;
+}
+
+void PaidLesson::showInfo() const {
+  cout << "Price: " << price << endl;
+  Lesson::showInfo();
+  // cout << "Theme: " << theme << endl;
+  // cout << "Info:  " << info << endl;
+  // cout << "Home:  " << getHome() << endl << endl;
+}
